9652baa2827d52b9a0a5dfaa5f5fa0a5ed01a806
Matt Brown <mattbrown@spotify.com>
2020-12-05 17:46:29 -0500

    problem 05 part 2

--- problem05/__main__.py
+++ problem05/__main__.py
@@ -8,4 +8,12 @@ if __name__ == "__main__":
     highest = max(seat_id(decode_seat(seat)) for seat in seats)
     print("part 1:", highest)
 
-    print("part 2:", "TODO")
+    all_seat_ids = sorted(seat_id(decode_seat(seat)) for seat in seats)
+    for i in range(len(all_seat_ids)):
+        if all_seat_ids[i + 1] != all_seat_ids[i] + 1:
+            my_id = all_seat_ids[i] + 1
+            break
+
+    assert my_id not in all_seat_ids
+
+    print("part 2:", my_id)
