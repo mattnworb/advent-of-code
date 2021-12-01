7d14e7922ece407308be91088e7e34962fc82182
Matt Brown <mattbrown@spotify.com>
2021-12-01 14:03:42 -0500

    2021 problem 01 part 2

--- 2021/problem01/__init__.py
+++ 2021/problem01/__init__.py
@@ -17,3 +17,54 @@ def part1(inp: str):
             increases += 1
         prev = cur
     return increases
+
+
+# Considering every single measurement isn't as useful as you expected: there's
+# just too much noise in the data.
+#
+# Instead, consider sums of a three-measurement sliding window. Again
+# considering the above example:
+#
+# ...
+#
+# Start by comparing the first and second three-measurement windows. The
+# measurements in the first window are marked A (199, 200, 208); their sum is
+# 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum
+# is 618. The sum of measurements in the second window is larger than the sum of
+# the first, so this first comparison increased.
+#
+# Your goal now is to count the number of times the sum of measurements in this
+# sliding window increases from the previous sum. So, compare A with B, then
+# compare B with C, then C with D, and so on. Stop when there aren't enough
+# measurements left to create a new three-measurement sum.
+#
+# ...
+#
+# Consider sums of a three-measurement sliding window. How many sums are larger
+# than the previous sum?
+def part2(inp: str):
+    increases = 0
+    this_window = []
+
+    measurements = [int(line) for line in inp.split("\n")]
+
+    for ix, m in enumerate(measurements):
+        # print(m)
+
+        # copy before append
+        prev_window = list(this_window)
+        this_window.append(m)
+
+        if len(this_window) > 3:
+            # print("filled up this window")
+
+            # trim
+            this_window = this_window[1:]
+
+            # print(f"comparing windows this={this_window} and prev={prev_window}")
+
+            if len(prev_window) == 3:
+                if sum(this_window) > sum(prev_window):
+                    increases += 1
+
+    return increases
--- 2021/problem01/test_problem01.py
+++ 2021/problem01/test_problem01.py
@@ -1,5 +1,16 @@
 from problem01 import *
 
 
-def test_part1_example():
-    pass
+def test_part2_example():
+    inp = """
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263""".strip()
+    assert part2(inp) == 5
