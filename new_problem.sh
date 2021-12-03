39ad6c4dcbd0ba9b40113003f42431a3d482862f
Matt Brown <mattbrown@spotify.com>
2021-12-03 08:58:29 -0500

    make script work for new layout

--- new_problem.sh
+++ new_problem.sh
@@ -2,27 +2,35 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [[ $# -lt 1 ]]; then
-    echo "Usage: $0 <directory>"
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <year> <day num>"
     exit 1
 fi
 
 # TODO: fix to allow specifying year too so this can be run from root dir
-name=$1
-mkdir "$name"
-touch "$name/__init__.py"
-cat <<END >"${name}/__init__.py"
+year=$1
+day=$2
+path="y${year}/problem${day}"
+pkg="y${year}.problem${day}"
+
+mkdir -p "${path}"
+touch "${path}/input"
+touch "${path}/__init__.py"
+cat <<END >"${path}/__init__.py"
 from typing import *
 
-def part1():
+def part1(inp: str):
+    pass
+
+def part2(inp: str):
     pass
 END
 
-cat <<END >"${name}/__main__.py"
-from $name import *
+cat <<END >"${path}/__main__.py"
+from ${pkg} import *
 
 if __name__ == "__main__":
-    with open("$name/input") as f:
+    with open("$path/input") as f:
         inp = f.read(-1).strip()
 
     print("part 1:", "TODO")
@@ -30,9 +38,12 @@ if __name__ == "__main__":
     print("part 2:", "TODO")
 END
 
-cat <<END >"${name}/test_$name.py"
-from $name import *
+cat <<END >"${path}/test_problem${day}.py"
+from $pkg import *
 
 def test_part1_example():
+    pass
+
+def test_part2_example():
     pass
 END
