3a313f75da2354bf4387480a7728607d313d7180
Matt Brown <mattbrown@spotify.com>
2022-12-02 09:50:34 -0500

    day 2 part 1

---
+++ y2022/problem02/test_problem02.py
@@ -0,0 +1,14 @@
+from y2022.problem02 import *
+
+example = """A Y
+B X
+C Z""".strip()
+
+
+def test_part1_example():
+    assert part1(example) == 15
+
+
+def test_part2_example():
+    # TODO: populate
+    assert part2(example)
