8d1e0fcc01bf35fb05146655e3ec0a1b331d2937
Matt Brown <mattbrown@spotify.com>
2022-12-03 21:14:11 -0500

    2021 day 13 part 2

--- y2021/problem13/__init__.py
+++ y2021/problem13/__init__.py
@@ -45,4 +45,58 @@ def part1(inp: str):
 
 
 def part2(inp: str):
-    pass
+    dots = set()
+
+    dotlines, foldlines = inp.split("\n\n")
+
+    for line in dotlines.split("\n"):
+        sp = line.split(",")
+        x = int(sp[0])
+        y = int(sp[1])
+        dots.add((x, y))
+
+    for line in foldlines.split("\n"):
+        sp = line[len("fold along ") :].split("=")
+        fold_dir = sp[0]
+        fold_pos = int(sp[1])
+
+        assert fold_dir in ("x", "y")
+
+        new_paper = set()
+        for dot in dots:
+            x, y = dot
+
+            if fold_dir == "y":
+                if y < fold_pos:
+                    new_paper.add((x, y))
+                elif y == fold_pos:
+                    # drop because we are folding on this line
+                    continue
+                else:
+                    # determine new y
+                    new_y = fold_pos - (y - fold_pos)
+                    new_paper.add((x, new_y))
+            elif fold_dir == "x":
+                if x < fold_pos:
+                    new_paper.add((x, y))
+                elif x == fold_pos:
+                    continue
+                else:
+                    new_x = fold_pos - (x - fold_pos)
+                    new_paper.add((new_x, y))
+        dots = new_paper
+
+    max_x = max(d[0] for d in dots)
+    max_y = max(d[1] for d in dots)
+
+    paper: List[str] = []
+    for y in range(max_y + 1):
+        line = ""
+        for x in range(max_x + 1):
+            if (x, y) in dots:
+                line += "#"
+            else:
+                line += "."
+        paper.append(line)
+    print("\n".join(paper))
+    return len(new_paper)
