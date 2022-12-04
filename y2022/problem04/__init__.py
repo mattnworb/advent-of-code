6a67d003e94d6b1960a064e560690587a9339625
Matt Brown <mattbrown@spotify.com>
2022-12-04 18:57:26 -0500

    2022 day 4 part 1

---
+++ y2022/problem04/__init__.py
@@ -0,0 +1,24 @@
+from typing import *
+
+
+def parse_input(inp: str) -> List[Tuple[Set[int], Set[int]]]:
+    pairs = []
+    for line in inp.split("\n"):
+        left, right = line.split(",")
+        l1, l2 = left.split("-")
+        r1, r2 = right.split("-")
+        pairs.append(
+            (
+                set(range(int(l1), int(l2) + 1)),
+                set(range(int(r1), int(r2) + 1)),
+            )
+        )
+    return pairs
+
+
+def part1(inp: str):
+    return sum(p1.issubset(p2) or p2.issubset(p1) for p1, p2 in parse_input(inp))
+
+
+def part2(inp: str):
+    pass
---
+++ y2022/problem04/test_problem04.py
@@ -0,0 +1,17 @@
+from y2022.problem04 import *
+
+example = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+
+def test_part1_example():
+    assert part1(example) == 2
+
+
+def test_part2_example():
+    # TODO: populate
+    assert part2(example)
