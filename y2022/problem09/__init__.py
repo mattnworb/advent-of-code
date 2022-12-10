96389bc8e695a7233923861122f440d645d5d091
Matt Brown <mattbrown@spotify.com>
2022-12-10 14:55:30 -0500

    2022 day 9 part 2

--- y2022/problem09/__init__.py
+++ y2022/problem09/__init__.py
@@ -2,9 +2,10 @@ from typing import *
 from enum import Enum
 
 Position = Tuple[int, int]
+Directions = List[Tuple[str, int]]
 
 
-def parse(inp: str) -> List[Tuple[str, int]]:
+def parse(inp: str) -> Directions:
     ds = []
     for line in inp.strip().split("\n"):
         sp = line.split(" ")
@@ -13,21 +14,17 @@ def parse(inp: str) -> List[Tuple[str, int]]:
     return ds
 
 
-def part1(inp: str):
-    # How many positions does the tail of the rope visit at least once?
-    directions = parse(inp)
-
-    head = tail = (0, 0)
+def simulate(directions: Directions, knotcount: int) -> int:
+    knots = [(0, 0) for n in range(knotcount)]
 
     visited: Set[Position] = set()
-    visited.add(tail)
+    visited.add((0, 0))
 
     for move in directions:
         d, count = move
-        for n in range(count):
-            hx, hy = head
-            tx, ty = tail
-
+        for _ in range(count):
+            # first determine new position for head knot
+            hx, hy = knots[0]
             if d == "R":
                 hx += 1
             elif d == "L":
@@ -39,51 +36,62 @@ def part1(inp: str):
             else:
                 raise ValueError("no direction " + d)
 
-            head = hx, hy
+            knots[0] = hx, hy
 
-            # figure out how to move tail
-            if hx == tx:  # same x
-                if hy == ty - 2:
-                    # move tail up
-                    ty -= 1
-                elif hy == ty + 2:
-                    # move down
-                    ty += 1
-            elif hy == ty:  # same y
-                if hx == tx - 2:
-                    # move left
-                    tx -= 1
-                elif hx == tx + 2:
-                    # move right
-                    tx += 1
+            # then move each subsequent knot
+            for n in range(1, len(knots)):
+                hx, hy = knots[n - 1]
+                tx, ty = knots[n]
 
-            # "Otherwise, if the head and tail aren't touching and aren't in the
-            # same row or column, the tail always moves one step diagonally to
-            # keep up"
-            # if head and tail are touching diagonally, that means the
-            # difference in x positions is 1 and the difference in y positions
-            # is 1
-            elif abs(hx - tx) > 1 or abs(hy - ty) > 1:
-                # move diagonally rather than checking each of the 4 diagonals -
-                # if head is above and to the right, above and to the left, etc
-                # - can just check and adjust x and y separately
-                if hx > tx:
-                    tx += 1
-                elif hx < tx:
-                    tx -= 1
+                # figure out how to move this knot
+                if hx == tx:  # same x
+                    if hy == ty - 2:
+                        # move tail up
+                        ty -= 1
+                    elif hy == ty + 2:
+                        # move down
+                        ty += 1
+                elif hy == ty:  # same y
+                    if hx == tx - 2:
+                        # move left
+                        tx -= 1
+                    elif hx == tx + 2:
+                        # move right
+                        tx += 1
 
-                if hy > ty:
-                    ty += 1
-                elif hy < ty:
-                    ty -= 1
+                # "Otherwise, if the head and tail aren't touching and aren't in the
+                # same row or column, the tail always moves one step diagonally to
+                # keep up"
+                # if head and tail are touching diagonally, that means the
+                # difference in x positions is 1 and the difference in y positions
+                # is 1
+                elif abs(hx - tx) > 1 or abs(hy - ty) > 1:
+                    # move diagonally rather than checking each of the 4 diagonals -
+                    # if head is above and to the right, above and to the left, etc
+                    # - can just check and adjust x and y separately
+                    if hx > tx:
+                        tx += 1
+                    elif hx < tx:
+                        tx -= 1
 
-            tail = tx, ty
+                    if hy > ty:
+                        ty += 1
+                    elif hy < ty:
+                        ty -= 1
+
+                knots[n] = tx, ty
 
             # remember where tail has been
-            visited.add(tail)
+            visited.add(knots[-1])
 
     return len(visited)
 
 
+def part1(inp: str):
+    directions = parse(inp)
+    return simulate(directions, 2)
+
+
 def part2(inp: str):
-    pass
+    directions = parse(inp)
+    return simulate(directions, 10)
--- y2022/problem09/test_problem09.py
+++ y2022/problem09/test_problem09.py
@@ -14,6 +14,15 @@ def test_part1_example():
     assert part1(example) == 13
 
 
+example2 = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+
+
 def test_part2_example():
-    # TODO: populate
-    assert part2(example)
+    assert part2(example2) == 36
