756f2f6deada86c0eaf4a664544ada20d7b8aa5a
Matt Brown <mattbrown@spotify.com>
2022-12-12 19:56:49 -0500

    2022 day 12 part 1
    implemented A* search based on the Wikipedia description, surprised it
    mostly worked off the bat ... probably because I did 2021 day 15 last
    week which I used Dijkstra's for

---
+++ y2022/problem12/__init__.py
@@ -0,0 +1,119 @@
+from typing import *
+from collections import defaultdict
+
+# You ask the device for a heightmap of the surrounding area (your puzzle
+# input). The heightmap shows the local area from above broken into a grid; the
+# elevation of each square of the grid is given by a single lowercase letter,
+# where a is the lowest elevation, b is the next-lowest, and so on up to the
+# highest elevation, z.
+
+# Also included on the heightmap are marks for your current position (S) and the
+# location that should get the best signal (E). Your current position (S) has
+# elevation a, and the location that should get the best signal (E) has
+# elevation z.
+
+# You'd like to reach E, but to save energy, you should do it in as few steps as
+# possible. During each step, you can move exactly one square up, down, left, or
+# right. To avoid needing to get out your climbing gear, the elevation of the
+# destination square can be at most one higher than the elevation of your
+# current square; that is, if your current elevation is m, you could step to
+# elevation n, but not to elevation o. (This also means that the elevation of
+# the destination square can be much lower than the elevation of your current
+# square.)
+
+# ...
+
+# What is the fewest steps required to move from your current position to the
+# location that should get the best signal?
+
+
+# ----
+
+# isn't this like 2021 day 15 again, with Dijkstra?
+
+# lets try A*
+
+Node = tuple[int, int]
+
+
+def solve(inp: str) -> int:
+    # try storing as a graph
+    lines = inp.split("\n")
+    max_y = len(lines)
+    max_x = len(lines[0])
+    height: Dict[Node, int] = {}
+    for y, line in enumerate(lines):
+        for x, ch in enumerate(line):
+            if ch == "S":
+                start = x, y
+            elif ch == "E":
+                end = x, y
+            else:
+                height[x, y] = ord(ch) - 96
+    height[start] = 1
+    height[end] = 26
+
+    # now build the graph: graph[(x,y)] is a list of positions you can move to
+    # from (x,y). if you can move from A to B it does not imply you can move
+    # from B to A
+    graph: Dict[Node, List[Node]] = defaultdict(list)
+    for p in height:
+        for neighbor in [
+            (p[0], p[1] - 1),
+            (p[0], p[1] + 1),
+            (p[0] - 1, p[1]),
+            (p[0] + 1, p[1]),
+        ]:
+            if neighbor in height:  # checking if neighbor is in grid
+                # the elevation of the destination square can be at most one
+                # higher than the elevation of your current square
+                if height[neighbor] - height[p] <= 1:
+                    graph[p].append(neighbor)
+
+    # now we search
+    def h(n: Node) -> int:
+        return abs(n[0] - end[0]) + abs(n[1] - end[1])
+
+    open_set = {start}  # TODO: priority queue
+
+    # came_from = {}
+
+    # g_score[node] is the cost of the cheapest path from start to node that we
+    # know of so far
+    g_score = {start: 0}
+    # f_score[node] = g_score[node] + h(node). "fScore[n] represents our current
+    # best guess as to how cheap a path could be from start to finish if it goes
+    # through n."
+    f_score = {start: h(start)}
+
+    while open_set:
+        current = min(
+            open_set & f_score.keys(), key=lambda n: f_score[n]
+        )  # TODO: priority queue
+        if current == end:
+            # done
+            return g_score[current]
+
+        open_set.remove(current)
+
+        for neighbor in graph[current]:
+            score = g_score[current] + 1  # cost of each move is 1
+            # 'not in g_score' test is because we are only recording scores
+            # we've found, not an initial state of infinity for each node
+            if neighbor not in g_score or score < g_score[neighbor]:
+                # this path is better than any previous one
+                g_score[neighbor] = score
+                f_score[neighbor] = score + h(neighbor)
+                if neighbor not in open_set:
+                    open_set.add(neighbor)
+
+    # oops
+    raise ValueError("couldn't find path")
+
+
+def part1(inp: str):
+    return solve(inp)
+
+
+def part2(inp: str):
+    pass
---
+++ y2022/problem12/test_problem12.py
@@ -0,0 +1,16 @@
+from y2022.problem12 import *
+
+example = """Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+
+
+def test_part1_example():
+    assert part1(example) == 31
+
+
+def test_part2_example():
+    # TODO: populate
+    assert part2(example)
