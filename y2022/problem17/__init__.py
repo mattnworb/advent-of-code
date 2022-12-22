ca8820fb42a64560a2a1fe0e383b1999132388b0
Matt Brown <mattbrown@spotify.com>
2022-12-21 21:09:30 -0500

    day 17 part 1 done

---
+++ y2022/problem17/__init__.py
@@ -0,0 +1,138 @@
+from typing import *
+
+
+# first thoughts: represent the "rocks" - positions on the board occupied by
+# the tetris pieces - as (x,y) tuples in a set, so that the board (set)
+# contains just the rock's positions. treat the bottom row of the board as
+# y=0, row above that as y=1, etc, so that as we add rows there is nothing
+# to adjust in the existing pieces/positions.
+#
+# represent the Tetris pieces as sets also, with rock coordinates relative to the bottom-left corner as (0, 0)
+Rock = Tuple[int, int]
+Shape = Set[Rock]
+
+
+def shapes() -> Iterator[Shape]:
+    while True:
+        # the #### piece:
+        yield {(0, 0), (1, 0), (2, 0), (3, 0)}
+        # the cross:
+        yield {(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)}
+        # the backwards L:
+        yield {(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)}
+        # the vertical line:
+        yield {(0, 0), (0, 1), (0, 2), (0, 3)}
+        # the square:
+        yield {(0, 0), (1, 0), (0, 1), (1, 1)}
+
+
+def move_right(shape: Shape, times: int = 1, board: Set[Rock] = set()):
+    """Adjust the coordinates of the shape so that it moves right `times` time, but not past the edge of the wall"""
+    max_x = max(x for x, y in shape)
+    # cannot move the right-most part of the shape past a value of 6, since the x-axis is bounded between [0,6]:
+
+    # if right-most pos is 6 and times is 1, should adjust times to 0
+    # if right-most pos is 5 and times is 2, should adjust times to 1
+    times = min(times, 6 - max_x)
+    if times <= 0:
+        return shape
+
+    for n in range(times):
+        # is anything blocking us?
+        s = {(x + 1, y) for x, y in shape}
+        if all((x, y) not in board for x, y in s):
+            shape = s
+        else:
+            break
+    return shape
+
+
+def move_left(shape: Shape, times: int = 1, board: Set[Rock] = set()):
+    """Adjust the coordinates of the shape so that it moves left `times` time, but not past the edge of the wall"""
+    min_x = min(x for x, y in shape)
+    # cannot move the left-most part of the shape past a value of 0, since the x-axis is bounded between [0,6]:
+
+    # if right-most pos is 0 and times is 1, should adjust times to 0
+    # if right-most pos is 1 and times is 2, should adjust times to 1
+    times = min(times, min_x)
+    if times <= 0:
+        return shape
+    for n in range(times):
+        # is anything blocking us?
+        s = {(x - 1, y) for x, y in shape}
+        if all((x, y) not in board for x, y in s):
+            shape = s
+        else:
+            break
+    return shape
+
+
+def parse_input(inp) -> Iterator[str]:
+    while True:
+        for ch in inp:
+            yield ch
+
+
+def part1(inp: str):
+
+    board: Set[Rock] = set()
+    max_height = 0
+
+    # The tall, vertical chamber is exactly seven units wide. Each rock appears
+    # so that its left edge is two units away from the left wall and its bottom
+    # edge is three units above the highest rock in the room (or the floor, if
+    # there isn't one).
+
+    shape_gen = shapes()
+    jet_gen = parse_input(inp)
+
+    # How many units tall will the tower of rocks be after 2022 rocks have stopped falling?
+    for rock_num in range(2022):
+        # new rock
+        shape = next(shape_gen)
+        # Each rock appears so that its left edge is two units away from the
+        # left wall and its bottom edge is three units above the highest rock in
+        # the room (or the floor, if there isn't one).
+        shape = move_right(shape, 2)
+        shape = {(x, y + max_height + 3) for x, y in shape}
+
+        # After a rock appears, it alternates between being pushed by a jet of
+        # hot gas one unit (in the direction indicated by the next symbol in the
+        # jet pattern) and then falling one unit down. If any movement would
+        # cause any part of the rock to move into the walls, floor, or a stopped
+        # rock, the movement instead does not occur. If a downward movement
+        # would have caused a falling rock to move into the floor or an
+        # already-fallen rock, the falling rock stops where it is (having landed
+        # on something) and a new rock immediately begins falling.
+
+        rock_is_moving = True
+
+        while rock_is_moving:
+            jet = next(jet_gen)
+            shape = (
+                move_right(shape, board=board)
+                if jet == ">"
+                else move_left(shape, board=board)
+            )
+
+            # can we move down? only if there are no rocks in the position below these rocks, and we are not at the floor
+            can_move_down = all(
+                (r[0], r[1] - 1) not in board and r[1] - 1 >= 0 for r in shape
+            )
+            if can_move_down:
+                shape = {(r[0], r[1] - 1) for r in shape}
+            else:
+                # if not, this rock rests
+                for rock in shape:
+                    assert (
+                        rock not in board
+                    ), f"collision after rock number {rock_num} came to rest"
+                    board.add(rock)
+                rock_is_moving = False
+                max_height = max(max_height, max(y for x, y in shape) + 1)
+
+    return max_height
+
+
+def part2(inp: str):
+    pass
---
+++ y2022/problem17/test_problem17.py
@@ -0,0 +1,60 @@
+from y2022.problem17 import *
+
+
+def test_move_right():
+    # the vertical line:
+    shape = {(0, 0), (0, 1), (0, 2), (0, 3)}
+    assert shape == move_right(shape, 0)
+
+    for x in range(1, 7):
+        shape = move_right(shape, 1)
+        assert shape == {(x, 0), (x, 1), (x, 2), (x, 3)}
+
+    assert shape == {(6, 0), (6, 1), (6, 2), (6, 3)}
+    # can't move any more
+    assert shape == move_right(shape, 1)
+    assert shape == move_right(shape, 2)
+    assert shape == move_right(shape, 3)
+    assert shape == move_right(shape, 4)
+    assert shape == move_right(shape, 5)
+    assert shape == move_right(shape, 6)
+
+    # try with a board
+    shape = {(0, 0), (1, 0), (2, 0), (3, 0)}  # horizontal line
+    board = {(5, 0)}  # can shift right once, but twice would be a collision
+    assert move_right(shape, 1, board) == move_right(shape, 2, board)
+
+
+def test_move_left():
+    # the vertical line:
+    shape = {(0, 0), (0, 1), (0, 2), (0, 3)}
+
+    # can't move
+    assert shape == move_left(shape, 1)
+
+    shape = {(6, 0), (6, 1), (6, 2), (6, 3)}
+
+    for x in range(5, -1, -1):
+        shape = move_left(shape, 1)
+        assert shape == {(x, 0), (x, 1), (x, 2), (x, 3)}
+
+    assert shape == {(0, 0), (0, 1), (0, 2), (0, 3)}
+    # can't move any more
+    assert shape == move_left(shape, 1)
+    assert shape == move_left(shape, 2)
+    assert shape == move_left(shape, 3)
+    assert shape == move_left(shape, 4)
+    assert shape == move_left(shape, 5)
+    assert shape == move_left(shape, 6)
+
+
+example = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"""
+
+
+def test_part1_example():
+    assert part1(example) == 3068
+
+
+def test_part2_example():
+    # TODO: populate
+    assert part2(example)
