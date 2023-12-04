22558544fd65cd725ea15ad29d5e275f0c115bf1
Matt Brown <mattbrown@spotify.com>
2023-12-04 09:38:36 -0500

    2023 day 4 part 2

--- y2023/problem04/__init__.py
+++ y2023/problem04/__init__.py
@@ -1,10 +1,10 @@
 from typing import *
+from collections import Counter
 
 
 def part1(inp: str):
     total = 0
     for line in inp.split("\n"):
-        # print(line)
         numbers = line.split(": ")[1]
         winning, have = numbers.split("| ")
         winners = {int(s) for s in winning.split()}
@@ -16,4 +16,24 @@ def part1(inp: str):
 
 
 def part2(inp: str):
-    pass
+    c: Counter[int] = Counter()
+    for line in inp.split("\n"):
+        card, numbers = line.split(": ")
+        # "Card NN"
+        card_num = int(card[5:])
+        c[card_num] += 1
+        winning, have = numbers.split("| ")
+        winners = {int(s) for s in winning.split()}
+        mine = {int(s) for s in have.split()}
+        num_winners = len(winners & mine)
+
+        # win one copy each of the cards after this one
+        # so if score is 2, win one copy of cards N+1 and N+2
+        # for x in range(c[card_num]):
+        for n in range(num_winners):
+            # don't add just 1, because we need to consider each copy of the
+            # card too and the winners it generates (by the time we get to card
+            # N in the input we have already figured out how many copies of it
+            # we have won from previous cards)
+            c[card_num + n + 1] += c[card_num]
+    return sum(c.values())
--- y2023/problem04/test_problem04.py
+++ y2023/problem04/test_problem04.py
@@ -13,5 +13,4 @@ def test_part1_example():
 
 
 def test_part2_example():
-    # TODO: populate
-    assert part2(example)
+    assert part2(example) == 30
