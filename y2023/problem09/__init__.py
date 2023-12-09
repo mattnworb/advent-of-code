ca400df80703cf181115ab4bed2095799000f800
Matt Brown <mattbrown@spotify.com>
2023-12-09 13:13:24 -0500

    2023 day 9 part 1
    recursive answer might not work for part2 I'm betting

---
+++ y2023/problem09/__init__.py
@@ -0,0 +1,27 @@
+from typing import *
+
+
+def calculate_next_value(nums: List[int]) -> int:
+    # initially was checking if the sum was 0 here, but that can break if the
+    # list contains postivie and negative numbers
+    if all(n == 0 for n in nums):
+        return 0
+
+    diff_between_values = [nums[i + 1] - nums[i] for i in range(len(nums) - 1)]
+
+    return nums[-1] + calculate_next_value(diff_between_values)
+
+
+# "Analyze your OASIS report and extrapolate the next value for each history.
+# What is the sum of these extrapolated values?"
+def part1(inp: str):
+    seqs: List[List[int]] = []
+    for line in inp.split("\n"):
+        seqs.append(list(map(int, line.split())))
+
+    next_vals = list(map(calculate_next_value, seqs))
+    return sum(next_vals)
+
+
+def part2(inp: str):
+    pass
---
+++ y2023/problem09/test_problem09.py
@@ -0,0 +1,21 @@
+from y2023.problem09 import *
+
+example = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+
+def test_part1_example():
+    assert part1(example) == 114
+
+    # is this right?
+    ex = "1 2 5 13 33 89 245 643 1565 3535 7495 15128 29479 56181 105913 199391 377649 723582 1407901 2788593 5627669"
+    assert part1(ex) == 11562151
+
+    ex = "0 -9 -18 -27"
+    assert part1(ex) == -36
+
+
+def test_part2_example():
+    # TODO: populate
+    assert part2(example)
