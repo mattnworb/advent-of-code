0c12dce41431eac72dc6eeb6efdc92eaaf1b5206
Matt Brown <mattbrown@spotify.com>
2023-12-15 15:17:54 -0500

    2023 day 14 part 1

---
+++ y2023/problem14/__init__.py
@@ -0,0 +1,55 @@
+from typing import *
+
+
+def move_rocks_and_score(line: str) -> int:
+    size = len(line)
+
+    # example:
+    # .O...#O..O
+
+    # scan through the line, counting how many round rocks (O) or empty spaces
+    # (.) we find until we hit a cube-shaped rock (#) once we hit a #, we can
+    # calculate that all the round rocks would move left as far as they can
+    num_round, last_cube = 0, -1
+    score = 0
+    for i in range(size):
+        if line[i] == "#":
+            # update score
+            # ..O#.O would become O..#.O
+            # last_cube=0, num_round=1, score += 6
+            # sum(range(size,size-num_round,-1))
+            # O..#.O would become O..#O.
+            # last_cube=3, num_round=1, score += 2
+            # sum(range(size-(last_cube+1),size-(last_cube+1)-num_round),-1)
+            # if last_cube > 0:
+            score += sum(
+                range(size - (last_cube + 1), size - (last_cube + 1) - num_round, -1)
+            )
+
+            last_cube = i
+            num_round = 0
+
+        elif line[i] == "O":
+            num_round += 1
+
+    # update one last time after the line terminates
+    score += sum(range(size - (last_cube + 1), size - (last_cube + 1) - num_round, -1))
+
+    return score
+
+
+def part1(inp: str):
+    grid: List[str] = []
+    for line in inp.split("\n"):
+        grid.append(line)
+
+    # transpose as its easier to work with left to right scanning one row at a time
+    columns = []
+    for c in range(len(grid[0])):
+        columns.append("".join(line[c] for line in grid))
+
+    return sum(map(move_rocks_and_score, columns))
+
+
+def part2(inp: str):
+    pass
---
+++ y2023/problem14/test_problem14.py
@@ -0,0 +1,22 @@
+from y2023.problem14 import *
+
+example = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""
+
+
+def test_part1_example():
+    assert move_rocks_and_score("..O#.O") == 8
+    assert part1(example) == 136
+
+
+def test_part2_example():
+    # TODO: populate
+    assert part2(example)
