682a3b06163ef14d7f266990e141b6fb162562a0
Matt Brown <mattbrown@spotify.com>
2023-12-19 22:03:44 -0500

    day 14 part 2 done - this was a headache

--- y2023/problem14/__init__.py
+++ y2023/problem14/__init__.py
@@ -1,101 +1,154 @@
 from typing import *
 
+Grid = list[list[str]]
 
-def move_rocks_north(grid: Sequence[Sequence[str]]) -> None:
+
+def tilt_north(grid: Grid) -> None:
     # mutate in-place
 
-    for line in grid:
-        prev_cube_ix = 0
-        num_round = 0
-        num_empty = 0
-        new_line = ""
-        for ix in range(len(line)):
-            if line[ix] == "O":
-                num_round += 1
-            elif line[ix] == ".":
-                num_empty += 1
-            elif line[ix] == "#":
-                new_line += "O" * num_round + "." * num_empty + "#"
-                prev_cube_ix = ix
-                num_round = 0
-                num_empty = 0
-        # add whatever is left after last #
-        if prev_cube_ix != len(line) - 1:
-            new_line += "O" * num_round + "." * num_empty
-        new_grid.append(new_line)
-    return new_grid
+    # scan down the rows of each column
+    for c in range(len(grid[0])):
+        empties: list[int] = []
+        for r in range(len(grid[c])):
+            if grid[r][c] == "O" and empties:
+                # swap
+                first_empty = empties.pop(0)
+                grid[first_empty][c] = "O"
+                grid[r][c] = "."
+                empties.append(r)
+            elif grid[r][c] == ".":
+                empties.append(r)
+            elif grid[r][c] == "#":
+                empties.clear()
 
 
-# we've inverted the problem so we are moving things to the left (and
-# transposing once before that) so the scoring has to be column based instead of
-# row based
-def score(grid: list[str]) -> int:
+# O....#....
+# O.OO#....#
+# .....##...
+# OO.#O....O
+# .O.....O#.
+# O.#..O.#.#
+# ..O..#O..O
+# .......O..
+# #....###..
+# #OO..#....
+
+
+def tilt_west(grid: Grid) -> None:
+    # scan left-to-right in each row
+    for r in range(len(grid)):
+        empties: list[int] = []
+        for c in range(len(grid[r])):
+            if grid[r][c] == "O" and empties:
+                # swap
+                first_empty = empties.pop(0)
+                grid[r][first_empty] = "O"
+                grid[r][c] = "."
+                empties.append(c)
+            elif grid[r][c] == ".":
+                empties.append(c)
+            elif grid[r][c] == "#":
+                empties.clear()
+
+
+def tilt_south(grid: Grid) -> None:
+    # scan UP the rows of each column
+    for c in range(len(grid[0])):
+        empties: list[int] = []
+        for r in range(len(grid[c]) - 1, -1, -1):
+            if grid[r][c] == "O" and empties:
+                # swap
+                first_empty = empties.pop(0)
+                grid[first_empty][c] = "O"
+                grid[r][c] = "."
+                empties.append(r)
+            elif grid[r][c] == ".":
+                empties.append(r)
+            elif grid[r][c] == "#":
+                empties.clear()
+
+
+def tilt_east(grid: Grid) -> None:
+    # scan right-to-left in each row
+    for r in range(len(grid)):
+        empties: list[int] = []
+        for c in range(len(grid[r]) - 1, -1, -1):
+            if grid[r][c] == "O" and empties:
+                # swap
+                first_empty = empties.pop(0)
+                grid[r][first_empty] = "O"
+                grid[r][c] = "."
+                empties.append(c)
+            elif grid[r][c] == ".":
+                empties.append(c)
+            elif grid[r][c] == "#":
+                empties.clear()
+
+
+def score(grid: Sequence[Sequence[str]]) -> int:
     total = 0
-    for line in grid:
-        for c, ch in enumerate(line):
-            if ch == "O":
-                total += len(line) - c
+    for r in range(len(grid)):
+        rows_from_south = len(grid) - r
+        total += sum(
+            [rows_from_south for c in range(len(grid[r])) if grid[r][c] == "O"]
+        )
     return total
 
 
-def transpose(grid: Sequence[str]) -> Sequence[str]:
-    columns = []
-    for c in range(len(grid[0])):
-        columns.append("".join(line[c] for line in grid))
-    return columns
-
-
 def part1(inp: str):
-    grid: List[str] = []
+    grid: Grid = []
     for line in inp.split("\n"):
-        grid.append(line)
+        grid.append(list(line))
 
-    # transpose as its easier to work with left to right scanning one row at a
-    # time when moving things
-    grid = move_rocks_left(transpose(grid))
+    tilt_north(grid)
     return score(grid)
 
 
-def rotate_cw(grid: list[str]) -> list[str]:
-    return list(map("".join, zip(*reversed(grid))))
-
-
-def one_spin_cycle(grid: Sequence[str]) -> list[str]:
-    # move north
-    grid = move_rocks_left(transpose(grid))
-    # move west
-    grid = move_rocks_left(transpose(rotate_cw(grid)))
-    # move south
-    grid = move_rocks_left(transpose(rotate_cw(grid)))
-    # move east
-    grid = move_rocks_left(transpose(rotate_cw(grid)))
-    # rotate back to original orientation
-    # return rotate_cw(grid)
-    return grid
+def one_spin_cycle(grid: Grid) -> None:
+    tilt_north(grid)
+    tilt_west(grid)
+    tilt_south(grid)
+    tilt_east(grid)
 
 
 def part2(inp: str):
+    grid: Grid = []
+    for line in inp.split("\n"):
+        grid.append(list(line))
+
     # "This process should work if you leave it running long enough, but you're
     # still worried about the north support beams. To make sure they'll survive
     # for a while, you need to calculate the total load on the north support
     # beams after 1000000000 cycles."
 
     # note: the number of cycles is how many times to move in all four
-    # directions, so we really move 4 trillion times total
-    cycles = 1_000_000_000  # one trillion
+    # directions, so we really move 4 billion times total
+    cycles = 1_000_000_000  # one billion
 
-    grid: List[str] = []
-    for line in inp.split("\n"):
-        grid.append(line)
+    # assume we don't want to actually iterate one billion times, and that
+    # after some number of spins, we'll be encountering states we've been at already
 
-    # in the problem text, everything moves "roll north, then west, then south,
-    # then east"
-    #
-    # to avoid rewriting my move() function, rolling west is really the same
-    # thing as rotating the grid clockwise then moving left, moving south is
-    # another rotation, etc.
+    GridSnapshot = tuple[tuple[str, ...], ...]
 
-    # one cycle
+    def snapshot() -> GridSnapshot:
+        return tuple(tuple(row) for row in grid)
 
-    # move, then spin
-    pass
+    past_states = {snapshot(): 0}
+    cyclenum_to_snapshot = {0: snapshot()}
+
+    for n in range(1, 1000):
+        one_spin_cycle(grid)
+
+        new_state = snapshot()
+        if new_state in past_states:
+            cycle_length = n - past_states[new_state]
+            # this took some trial and error to stumble upon:
+            #
+            # we've done n iterations so far, and we have (cycles - n) left to do
+            # where in the loop will we be after we do (cycles - n) more?
+
+            prev_iteration = past_states[new_state] + ((cycles - n) % cycle_length)
+            return score(cyclenum_to_snapshot[prev_iteration])
+
+        past_states[new_state] = n
+        cyclenum_to_snapshot[n] = new_state
--- y2023/problem14/test_problem14.py
+++ y2023/problem14/test_problem14.py
@@ -12,36 +12,52 @@ O.#..O.#.#
 #OO..#...."""
 
 
-def test_move_rocks():
-    assert move_rocks_left(["O...O#O"]) == ["OO...#O"]
-    assert move_rocks_left(["#.#OO#..O", "..O#..#.O"]) == ["#.#OO#O..", "O..#..#O."]
+# def test_move_rocks():
+#     assert move_rocks_left(["O...O#O"]) == ["OO...#O"]
+#     assert move_rocks_left(["#.#OO#..O", "..O#..#.O"]) == ["#.#OO#O..", "O..#..#O."]
 
 
-def test_transpose():
-    # 12
-    # 34
-    assert transpose(["12", "34"]) == ["13", "24"]
+# def test_transpose():
+#     # 12
+#     # 34
+#     assert transpose(["12", "34"]) == ["13", "24"]
 
-    m = ["12", "34", "56"]
-    assert transpose(m) == ["135", "246"]
-    assert transpose(transpose(m)) == m
+#     m = ["12", "34", "56"]
+#     assert transpose(m) == ["135", "246"]
+#     assert transpose(transpose(m)) == m
 
 
-def test_rotate_cw():
-    assert rotate_cw(["12", "34"]) == ["31", "42"]
-    assert rotate_cw(rotate_cw(rotate_cw(rotate_cw(["12", "34"])))) == ["12", "34"]
+# def test_rotate_cw():
+#     assert rotate_cw(["12", "34"]) == ["31", "42"]
+#     assert rotate_cw(rotate_cw(rotate_cw(rotate_cw(["12", "34"])))) == ["12", "34"]
+
+
+def test_tilt_north():
+    g = [list(line) for line in example.split("\n")]
+    tilt_north(g)
+
+    expected = """OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#...."""
+    assert g == [list(line) for line in expected.split("\n")]
 
 
 def test_part1_example():
     assert part1(example) == 136
 
 
-def test_part2_building_blocks():
-    g = one_spin_cycle(example.split("\n"))
-    assert (
-        "\n".join(g)
-        == """
-.....#....
+def test_one_spin_cycle():
+    g = [list(line) for line in example.split("\n")]
+    one_spin_cycle(g)
+
+    expected = """.....#....
 ....#...O#
 ...OO##...
 .OO#......
@@ -50,11 +66,9 @@ def test_part2_building_blocks():
 ....O#....
 ......OOOO
 #...O###..
-#..OO#....""".strip()
-    )
+#..OO#...."""
+    assert g == [list(line) for line in expected.split("\n")]
 
 
 def test_part2_example():
-    # TODO: populate
-    # assert part2(example)
-    pass
+    assert part2(example) == 64
